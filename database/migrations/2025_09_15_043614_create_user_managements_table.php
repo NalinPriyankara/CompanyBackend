2711c0a32ab4d12558361e1d73f74274807b7e5e
NalinPriyankara <nalinpriyankara033@gmail.com>
2026-01-23 09:56:48 +0530

    init

---
+++ database/migrations/2025_09_15_043614_create_user_managements_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_managements', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('department')->nullable();
+            $table->string('telephone', 10)->nullable();
+            $table->string('address')->nullable();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_managements');
+    }
+};
